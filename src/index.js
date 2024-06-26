a77c7db3473363da724bcecf37486e6901f1099d
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2024-06-26 20:04:42 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const client = new MercadoPagoConfig({ accessToken: "APP_USR-8214833007776308-04
 
 app.get("/pruebapagina", async (req, res)=>{
     res.status(200).send("pagina funcionando");
-   console.log(MPKEY)
+   console.log(PORT)
 })
 
 app.post("/create_preference/:user/:id/:saldo/:usercomp", async (req, res)=> {
