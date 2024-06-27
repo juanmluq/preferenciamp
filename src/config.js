a70364d36489b903c91b0a55e378ae0f9d82f6bd
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2024-06-26 22:22:30 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const DB_PORT = process.env.DB_PORT || 3000; 
-export const MP_KEY = process.env.MP_KEY || 2545;
+export const PORT = process.env.PORT || 3000; 
+export const MP_KEY = process.env.MP_KEY;
 export const PRUEBA = process.env.PRUEBA
 
